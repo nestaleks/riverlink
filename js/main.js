0b728a723532b7f8916565069716868d2d9cc263
nestaleks <nestaleks83@gmail.com>
2024-02-11 15:06:36 +0200

    1

--- js/main.js
+++ js/main.js
@@ -12,6 +12,24 @@ menuClose.addEventListener('click', ()=>{
     menuShadow.classList.remove('menu-open');
 });
 
+//кнопка Наверх
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("upBtn").style.display = "block";
+    } else {
+        document.getElementById("upBtn").style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
 //документи
 $(document).ready(function() {
 	$('.popup__link').magnificPopup({
